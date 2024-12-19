efc5cab894d9996908a6a9ed9a919711dda8eb85
Jessy <143501370+jessygomes@users.noreply.github.com>
2024-12-19 20:26:14 +0100

    Update Social.tsx

--- components/Auth/Social.tsx
+++ components/Auth/Social.tsx
@@ -4,7 +4,6 @@
 import { signIn } from "next-auth/react";
 import { FcGoogle } from "react-icons/fc";
 import { DEFAULT_LOGIN_REDIRECT } from "@/route";
-import { BottomGradient } from "../ui/BottomGradient";
 
 export function Social() {
   const onClick = (provider: "google") => {
@@ -14,12 +13,12 @@ export function Social() {
   return (
     <div className="flex items-center w-full gap-x-2">
       <button
-        className="relative group/btn flex space-x-2 items-center justify-center px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-500"
+        className="relative group/btn flex space-x-2 items-center justify-center px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-noir-900 hover:bg-noir-800 duration-300"
         onClick={() => onClick("google")}
       >
         <FcGoogle className="h-5 w-5" />
-        <BottomGradient />
       </button>
     </div>
   );
 }
+
