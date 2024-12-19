cd77c9a99f899b6b4bf6effbc7fc8016da056266
Jessy <143501370+jessygomes@users.noreply.github.com>
2024-12-19 20:25:13 +0100

    Update CardWrapper.tsx

--- components/Auth/CardWrapper.tsx
+++ components/Auth/CardWrapper.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { BackButton } from "./BackBtn";
 import { Social } from "./Social";
 
@@ -18,9 +17,9 @@ export const CardWrapper = ({
   showSocial = true,
 }: CardWrapperProps) => {
   return (
-    <div className="flex flex-col gap-4 w-2/5 p-2 bg-noir">
+    <div className="flex flex-col gap-4 p-2 bg-noir">
       <div className="flex justify-between items-center">
-        <h1 className="text-xl font-bold text-white text-center w-full">
+        <h1 className="text-xl font-bold text-white text-center uppercase w-full">
           {headerLabel}
         </h1>
       </div>
@@ -36,12 +35,7 @@ export const CardWrapper = ({
       <div>
         <BackButton label={backButtonLabel} href={backButtonHref} />
       </div>
-      <Link
-        className="text-center text-noir-100 text-xs hover:text-white/70 transition-all ease-in-out duration-150"
-        href="/auth/reset"
-      >
-        Mot de passe oublié ?
-      </Link>
     </div>
   );
 };
+
